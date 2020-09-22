dc6582159a0eaea5275c3153c323023e305ddebe
Byeonggon Lee <gonny952@gmail.com>
2020-09-22 21:45:16 +0900

    Add nugurang backend directory

---
+++ nugurang-back/src/main/java/com/nugurang/nugurang/NugurangApplication.java
@@ -0,0 +1,13 @@
+package com.nugurang.nugurang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NugurangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NugurangApplication.class, args);
+	}
+
+}
---
+++ nugurang-back/src/test/java/com/nugurang/nugurang/NugurangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nugurang.nugurang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NugurangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
