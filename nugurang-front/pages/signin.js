2f8fe325ea35e9085ec6cb995dcf524e2166ed9d
Byeonggon Lee <gonny952@gmail.com>
2020-09-23 11:37:52 +0900

    nugurang-front: Add example signin.js

--- nugurang-front/pages/signin.js
+++ nugurang-front/pages/signin.js
@@ -2,11 +2,9 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Footer from '../components/Footer';
 
-
 const useStyles = makeStyles((theme) => ({
 }));
 
 export default function singin() {
   return (<Footer/>);
 }
-
