c0a2e278715472ef6697fedb75733341cd616582
Byeonggon Lee <gonny952@gmail.com>
2020-11-16 19:09:59 +0900

    Revert "Add @NoArgsConstructor to all dtos"
    This reverts commit 0ea41b04ef643a4fd50a117cc095e46132a5b395.

--- nugurang-back/src/main/java/com/nugurang/dto/BoardDto.java
+++ nugurang-back/src/main/java/com/nugurang/dto/BoardDto.java
@@ -4,11 +4,9 @@ import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 @Builder
 public class BoardDto {
     @NotNull
--- nugurang-back/src/main/java/com/nugurang/dto/EventDto.java
+++ nugurang-back/src/main/java/com/nugurang/dto/EventDto.java
@@ -5,11 +5,9 @@ import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 @Builder
 public class EventDto {
     @NotNull
