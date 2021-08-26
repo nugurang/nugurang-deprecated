0a17b65d2adad1584727aa01d9c9cf3f42856205
Byeonggon Lee <gonny952@gmail.com>
2021-08-26 02:06:11 +0000

    Remove hardcoding

--- nugurang-back/src/main/java/com/nugurang/controller/AfterSigninController.java
+++ nugurang-back/src/main/java/com/nugurang/controller/AfterSigninController.java
@@ -3,16 +3,21 @@ package com.nugurang.controller;
 import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RequiredArgsConstructor
 @RestController
 public class AfterSigninController {
 
+    @Value("${nugurang.addr.front.url}")
+    private String frontUrl;
+
     @RequestMapping("/after-signin")
     public void afterSignin(HttpServletResponse response, @CookieValue("JSESSIONID") String jsessionId) throws IOException {
-        response.sendRedirect("http://localhost:3000/after-signin/" + jsessionId);
+        response.sendRedirect(frontUrl + "/after-signin/" + jsessionId);
     }
 }
--- nugurang-back/src/main/java/com/nugurang/controller/AfterSignoutController.java
+++ nugurang-back/src/main/java/com/nugurang/controller/AfterSignoutController.java
@@ -3,15 +3,20 @@ package com.nugurang.controller;
 import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RequiredArgsConstructor
 @RestController
 public class AfterSignoutController {
 
+    @Value("${nugurang.addr.front.url}")
+    private String frontUrl;
+
     @RequestMapping("/after-signout")
     public void afterSignout(HttpServletResponse response) throws IOException {
-        response.sendRedirect("http://localhost:3000/");
+        response.sendRedirect(frontUrl);
     }
 }
